7a9499d9e75795af10e7e7edc0aa8e6cd557028c
Jertusz <jendziu@gmail.com>
2020-02-15 23:59:23 +0100

    version 0.1

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 
-version = '0.1.1'
+version = '0.1'
 
 setup(
-    name='tf2-sku',
-    packages=['tf2-sku'],
+    name='tf2sku',
+    packages=['tf2sku'],
     version=version,
-    description='Python lib for creating sku',
+    description='Python lib for creating sku for tf2 items',
     author='Jędrzej Szadejko',
     author_email='shadyprywatny@gmail.com',
     license='MIT',
     url='https://github.com/Jertusz/python-tf2-sku',
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    download_url='https://github.com/Jertusz/python-tf2-sku',
     keywords=['steam', 'trade', 'tf2', 'teamfortress2' ],
     classifiers=[
     'Development Status :: 3 - Alpha',
